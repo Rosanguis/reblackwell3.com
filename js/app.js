c86a28ab9f831a926038ac551527a9553bd363f9
reblackwell3 <reblackwell3@gmail.com>
2015-09-08 20:47:42 -0500

    added opacity effect to square image grid

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ $(document)
             equalize_on_stack: true
         }
     });
+
+var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
+for (var i = 0; i < articleIds.length; i++) {
+    $(articleIds[i] + ' ul.small-block-grid-2').find('img:eq(' + i + ')').css('opacity', '1');
+};
