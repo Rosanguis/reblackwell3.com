8bfd02ac8402b2545c20034bb5c2a17516d5de47
reblackwell3 <reblackwell3@gmail.com>
2015-09-11 18:39:08 -0500

    grunt configuration for livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,69 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed',
-          sourceMap: true,
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed',
+                    sourceMap: true
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
         },
-        files: {
-          'css/app.css': 'scss/app.scss'
+        // grunt-express will serve the files from the folders listed in `bases`
+        // on specified `port` and `hostname`
+        express: {
+            all: {
+                options: {
+                    port: 9000,
+                    hostname: '0.0.0.0',
+                    bases: ['C:\\Users\\HP\\Desktop\\robert3blackwell.com'], // Replace with the directory you want the files served from
+                    // Make sure you don't use `.` or `..` in the path as Express
+                    // is likely to return 403 Forbidden responses if you do
+                    // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            grunt: {
+                options: {
+                    reload: true
+                },
+                files: ['Gruntfile.js']
+            },
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass']
+            },
+            all: {
+                files: ['**/*.html', '**/*.*css'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        // grunt-open will open your browser at the project's URL
+        open: {
+            all: {
+                // Gets the port from the connect configuration
+                path: 'http://localhost:<%= express.all.options.port%>/index.html'
+            }
         }
-      }
-    },
-
-    watch: {
-      grunt: {
-        options: {
-          reload: true
-        },
-        files: ['Gruntfile.js']
-      },
-
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
-}
+    
+    });
+    // Load Grunt tasks declared in the package.json file
+    require('matchdep')
+        .filterDev('grunt-*')
+        .forEach(grunt.loadNpmTasks);
+    // grunt.loadNpmTasks('grunt-sass');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('build', ['sass']);
+    grunt.registerTask('server', ['express', 'open', 'watch']);
+    grunt.registerTask('default', ['build', 'server', 'watch']);
+};
