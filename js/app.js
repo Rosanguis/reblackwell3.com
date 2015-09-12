d62882df1f324713cfeb037c7dd7125296d8e522
reblackwell3 <reblackwell3@gmail.com>
2015-09-12 17:52:04 -0500

    toggling for navbar active, separate about and contact pages

--- js/app.js
+++ js/app.js
@@ -6,20 +6,45 @@ $(document)
             equalize_on_stack: true
         }
     });
+
+function buildHandlebarHtml(articleId) {
+    var handlebarScript = $(articleId)
+        .find('script');
+    var template = Handlebars.compile(handlebarScript.html());
+    var templateHtml = template();
+    handlebarScript.after(templateHtml);
+}
+
+function setActiveTab(activeTabSelector) {
+    $('#ul-nav li')
+        .removeClass('active');
+    $(activeTabSelector).addClass('active');
+}
+
+function displayContactPage() {
+    var hideElements = $('body')
+        .children()
+        .not('#topbar', '#contact');
+    hideElements.hide();
+    $('body')
+        .find('#contact')
+        .show();
+    setActiveTab('#contact-nav');
+}
+
+function displayAboutPage() {
+    var hideElements = $('#contact');
+    hideElements.hide();
+    $('body')
+        .children()
+        .not('#contact')
+        .show();
+    setActiveTab('#about-nav');
+}
 $(document)
     .ready(function () {
         Handlebars.registerPartial('article-header', $('#article-header')
             .html());
-
-        function buildHandlebarHtml(articleId) {
-            var handlebarScript = $(articleId)
-                .find('script');
-            var template = Handlebars.compile(handlebarScript.html());
-            var templateHtml = template();
-            handlebarScript
-                .after(templateHtml);
-        }
-        
         var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
         for (var i = 0; i < articleIds.length; i++) {
             var articleId = articleIds[i];
