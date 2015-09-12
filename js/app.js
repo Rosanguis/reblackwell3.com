839d1a3698dfa8c1d207fbd84680213d39cf2e10
reblackwell3 <reblackwell3@gmail.com>
2015-09-12 13:45:33 -0500

    Used Handlebars.js to create uniform article headers

--- js/app.js
+++ js/app.js
@@ -6,10 +6,26 @@ $(document)
             equalize_on_stack: true
         }
     });
+$(document)
+    .ready(function () {
+        Handlebars.registerPartial('article-header', $('#article-header')
+            .html());
 
-var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
-for (var i = 0; i < articleIds.length; i++) {
-    $(articleIds[i] + ' ul.small-block-grid-2')
-        .find('img:eq(' + i + ')')
-        .css('opacity', '1');
-}
+        function buildHandlebarHtml(articleId) {
+            var handlebarScript = $(articleId)
+                .find('script');
+            var template = Handlebars.compile(handlebarScript.html());
+            var templateHtml = template();
+            handlebarScript
+                .after(templateHtml);
+        }
+        
+        var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
+        for (var i = 0; i < articleIds.length; i++) {
+            var articleId = articleIds[i];
+            buildHandlebarHtml(articleId);
+            $(articleId + ' ul.small-block-grid-2')
+                .find('img:eq(' + i + ')')
+                .css('opacity', '1');
+        }
+    });
