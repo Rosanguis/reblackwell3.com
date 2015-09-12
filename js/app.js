9bc2133e14ad0c2aaf16628df002c8c33724929c
reblackwell3 <reblackwell3@gmail.com>
2015-09-12 10:19:01 -0500

    Deleted files that promote bad practice

--- js/app.js
+++ js/app.js
@@ -9,5 +9,7 @@ $(document)
 
 var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
 for (var i = 0; i < articleIds.length; i++) {
-    $(articleIds[i] + ' ul.small-block-grid-2').find('img:eq(' + i + ')').css('opacity', '1');
-};
+    $(articleIds[i] + ' ul.small-block-grid-2')
+        .find('img:eq(' + i + ')')
+        .css('opacity', '1');
+}
