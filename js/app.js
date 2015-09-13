88d31d59cc1b996660418d81861525490e52cfdc
reblackwell3 <reblackwell3@gmail.com>
2015-09-13 14:20:22 -0500

    Created js function to center text vertically and changed html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 $(document)
     .foundation();
+
 function equalizeDocument() {
     $(document)
-    .foundation({
-        equalizer: {
-            equalize_on_stack: true
-        }
-    });
+        .foundation({
+            equalizer: {
+                equalize_on_stack: true
+            }
+        });
 };
 equalizeDocument();
 
@@ -47,16 +48,28 @@ function displayAboutPage() {
     setActiveTab('#about-nav');
     equalizeDocument();
 }
+
+function centerTextVerticallyWithImage() {
+    $('.center-vertically').each(function() {
+        var $closestImg = $(this).closest('.row').find('img');
+        var margin = ($closestImg.height() - $(this).height()) / 2;
+        $(this).css('margin', margin + 'px 0px');
+    });
+}
+
 $(document)
-    .ready(function () {
+    .ready(function() {
         Handlebars.registerPartial('article-header', $('#article-header')
             .html());
         var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
         for (var i = 0; i < articleIds.length; i++) {
             var articleId = articleIds[i];
             buildHandlebarHtml(articleId);
-            $(articleId + ' ul.small-block-grid-2')
-                .find('img:eq(' + i + ')')
+            $(articleId + ' ul.small-block-grid-2').find('img:eq(' + i + ')')
                 .css('opacity', '1');
         }
+        centerTextVerticallyWithImage();
+        $(window).resize(function() {
+            centerTextVerticallyWithImage();
+        });
     });
