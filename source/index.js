3d99f027343749f5fb1503cb1362f92f4cc6a618
reblackwell3 <reblackwell3@gmail.com>
2015-09-20 20:42:50 -0500

    reformatted index.js

--- source/index.js
+++ source/index.js
@@ -1,57 +1,57 @@
 $(document)
-    .foundation();
+  .foundation();
 
 function setActiveTab(activeTabSelector) {
-    $('#ul-nav li')
-        .removeClass('active');
-    $(activeTabSelector)
-        .addClass('active');
+  $('#ul-nav li')
+    .removeClass('active');
+  $(activeTabSelector)
+    .addClass('active');
 }
 
 function displayContactPage() {
-    var hideElements = $('body')
-        .children()
-        .not('#topbar', '#contact');
-    hideElements.hide();
-    $('body')
-        .find('#contact')
-        .show();
-    setActiveTab('#contact-nav');
+  var hideElements = $('body')
+    .children()
+    .not('#topbar', '#contact');
+  hideElements.hide();
+  $('body')
+    .find('#contact')
+    .show();
+  setActiveTab('#contact-nav');
 }
 
 function displayAboutPage() {
-    var hideElements = $('#contact');
-    hideElements.hide();
-    $('body')
-        .children()
-        .not('#contact')
-        .show();
-    setActiveTab('#about-nav');
+  var hideElements = $('#contact');
+  hideElements.hide();
+  $('body')
+    .children()
+    .not('#contact')
+    .show();
+  setActiveTab('#about-nav');
 }
 
 function centerTextVerticallyWithImage() {
-    $('.center-vertically')
-        .each(function () {
-            var $closestImg = $(this)
-                .closest('.row')
-                .find('img:visible');
-            var margin = ($closestImg.height() - $(this)
-                .height()) / 2;
-            $(this)
-                .css('margin', margin + 'px 0px');
-        });
+  $('.center-vertically')
+    .each(function () {
+      var $closestImg = $(this)
+        .closest('.row')
+        .find('img:visible');
+      var margin = ($closestImg.height() - $(this)
+        .height()) / 2;
+      $(this)
+        .css('margin', margin + 'px 0px');
+    });
 }
 $(window).load(function () {
-        var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
-        for (var i = 0; i < articleIds.length; i++) {
-            var articleId = articleIds[i];
-            $(articleId + ' ul.small-block-grid-2')
-                .find('img:eq(' + i + ')')
-                .css('opacity', '1');
-        }
-        centerTextVerticallyWithImage();
-        $(window)
-            .resize(function() {
-                centerTextVerticallyWithImage();
-            });
+  var articleIds = ['#about-georgia-tech', '#about-union-pacific', '#about-global-travel', '#about-eagle-scout'];
+  for (var i = 0; i < articleIds.length; i++) {
+    var articleId = articleIds[i];
+    $(articleId + ' ul.small-block-grid-2')
+      .find('img:eq(' + i + ')')
+      .css('opacity', '1');
+  }
+  centerTextVerticallyWithImage();
+  $(window)
+    .resize(function () {
+      centerTextVerticallyWithImage();
     });
+});
