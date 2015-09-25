12726c6b815cbe6b3b94d3fc4494bffcfec0a353
reblackwell3 <reblackwell3@gmail.com>
2015-09-24 20:54:56 -0500

    changed grunt build process and added base64

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,15 @@ module.exports = function (grunt) {
         tasks: ['processhtml']
       }
     },
+    base64: {
+      your_target: {
+        // Target-specific file lists and/or options go here. 
+        files: {
+          'source/shared/html_partials/_mid-build/gold_headshot.b64': 'resources/images/brand/gold_headshot.jpg',
+          'source/shared/html_partials/_mid-build/tech_tower.b64': 'resources/images/about/image-grid/tech_tower.jpg'
+        }
+      },
+    },
     // grunt-open will open your browser at the project's URL
     open: {
       index: {
@@ -74,6 +83,22 @@ module.exports = function (grunt) {
       src: ['source/*/*.html']
     },
     processhtml: {
+      html_partials: {
+        files: [{
+          expand: true,
+          cwd: './',
+          src: ['source/*/html_partials/_mid-build/*.html'],
+          dest: 'target/',
+          ext: '.html',
+          rename: function (dest, src) {
+            console.log(src);
+            var pathAfterSource = src.substr(src.indexOf('/') + 1);
+            var folderName = pathAfterSource.slice(0, pathAfterSource.indexOf('/')+1);
+            var fileName = src.substr(src.lastIndexOf('/'));
+            return dest + folderName + fileName;
+          }
+        }]
+      },
       dist: {
         files: [{
           expand: true,
@@ -81,7 +106,7 @@ module.exports = function (grunt) {
           src: ['source/*/*.html'],
           dest: 'target/',
           ext: '.html',
-          rename: function(dest, src) {
+          rename: function (dest, src) {
             return dest + src.substr(src.indexOf('/'));
           }
         }]
@@ -96,10 +121,10 @@ module.exports = function (grunt) {
 
         },
         files: {
-          'source/mid_build/foundation.min.js': ['bower_components/foundation/js/foundation/foundation.accordion.js', 'bower_components/foundation/js/foundation/foundation.equalizer.js', 'bower_components/foundation/js/foundation/foundation.topbar.js'],
-          'source/mid_build/modernizr.min.js': ['bower_components/foundation/js/vendor/modernizr.js'],
-          'source/mid_build/foundation_index.min.js': ['source/mid_build/foundation.min.js', 'source/index.js'],
-          'source/mid_build/foundation_intro.min.js': ['source/mid_build/foundation.min.js', 'source/intro.js']
+          'source/shared/html_partials/_mid-build/foundation.min.js': ['bower_components/foundation/js/foundation/foundation.accordion.js', 'bower_components/foundation/js/foundation/foundation.equalizer.js', 'bower_components/foundation/js/foundation/foundation.topbar.js'],
+          'source/shared/html_partials/_mid-build/modernizr.min.js': ['bower_components/foundation/js/vendor/modernizr.js'],
+          'source/shared/html_partials/_mid-build/foundation_after-intro.min.js': ['source/shared/html_partials/mid_build/foundation.min.js', 'source/shared/after-intro.js'],
+          // 'source/shared/html_partials/_mid-build/foundation_intro.min.js': ['source/shared/html_partials/mid_build/foundation.min.js', 'source/intro.js']
         }
       }
     },
@@ -107,16 +132,35 @@ module.exports = function (grunt) {
       about: 'target/about.html',
       slides: 'target/slides.html',
       contact: 'target/contact-form.html'
-    }
+    },
+    base64HtmlFiles: ['source/shared/html_partials/html-header.html', 'source/shared/html_partials/top-bar.html', 'source/intro/first-slide-body.html']
   });
   // Load Grunt tasks declared in the package.json file
   require('matchdep')
     .filterDev('grunt-*')
     .forEach(grunt.loadNpmTasks);
+  grunt.registerTask('inline-base64', function () {
+    var files = grunt.config('base64HtmlFiles'),
+      i;
+
+    for (i = 0; i < files.length; i++) {
+      var file = files[i];
+      var html = grunt.file.read(file);
+      var base64images = html.match('source.+_mid-build.+\.b64');
+      var j;
+      for (j = 0; j < base64images.length; j++) {
+        var path = base64images[j];
+        var base64 = 'data:image/jpg;base64,' + grunt.file.read(path);
+        html = html.replace(path, base64);
+      }
+      var newFileName = file.slice(0, file.lastIndexOf('/')) + '/_mid-build' + file.substr(file.lastIndexOf('/'));
+      grunt.file.write(newFileName, html);
+    }
+
+  });
   grunt.registerTask('build-json', function () {
     var project = grunt.file.readJSON('source/everything.json');
     var jsonKeyFileNameMap = grunt.config('jsonKeyFileNameMap');
-    console.log(project, jsonKeyFileNameMap);
     Object.keys(jsonKeyFileNameMap).forEach(function (jsonKey) {
       if (!grunt.file.exists(jsonKeyFileNameMap[jsonKey])) {
         grunt.log.error("file " + jsonKeyFileNameMap[jsonKey] + " not found");
@@ -126,7 +170,7 @@ module.exports = function (grunt) {
     });
     grunt.file.write('target/everything.json', JSON.stringify(project, null, 2)); //serialize it back to file
   });
-  grunt.registerTask('build', ['sass', 'uglify', 'processhtml']);
+  grunt.registerTask('build', ['base64', 'sass', 'uglify', 'inline-base64', 'processhtml']);
   grunt.registerTask('server', ['express', 'open', 'watch']);
   grunt.registerTask('default', ['build', 'build-json', 'server']);
 };
